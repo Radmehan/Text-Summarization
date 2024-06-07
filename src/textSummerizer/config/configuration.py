e63a5e315152f47a7567d6c7b559f118c752655f
Radmehan <74257580+Radmehan@users.noreply.github.com>
2024-06-07 20:18:22 +0600

    data tranformation

--- src/textSummerizer/config/configuration.py
+++ src/textSummerizer/config/configuration.py
@@ -1,6 +1,6 @@
 from textSummerizer.constants import *
 from textSummerizer.utils.common import read_yaml, create_directories
-from textSummerizer.entity import DataIngestionConfig, DataValidationConfig
+from textSummerizer.entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 
 
 class ConfigurationManager:
@@ -44,3 +44,18 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+
+    #data transformation
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name = config.tokenizer_name
+        )
+
+        return data_transformation_config
